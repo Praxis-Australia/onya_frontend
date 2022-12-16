267b18e6509bc74d58a7bcbe21111441b9664e00
Brian Lee <briancmlee23@gmail.com>
2022-12-17 05:04:44 +1100

    Add phone login/signup on separate page

--- dashboardui/lib/util/my_login.dart
+++ dashboardui/lib/util/my_login.dart
@@ -9,121 +9,88 @@ class MyLogin extends StatefulWidget {
 }
 
 class _MyLoginState extends State<MyLogin> {
-    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-    final TextEditingController _mobileController = TextEditingController();
-    final TextEditingController _otpController = TextEditingController();
-    bool _success = false;
-    bool _otpSent = false;
-    Exception? _errorMessage;
-    ConfirmationResult? confirmationResult;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _success = false;
+  String? _userEmail;
+  String? _userID;
+  Exception? _errorMessage;
+  User? user;
 
-    final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-    void _login() async {
-        _success = true;
-        
-        ConfirmationResult? confirmationResult = await _auth.signInWithPhoneNumber(_mobileController.text);
-        } on Exception catch (e) {
-            setState(() {
-            _errorMessage = e;
-            print(e);
-            print("there was an exception");
-            });
-        }
-
+  void _login() async {
+    _success = true;
+    try {
+      final User? user = (await _auth.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      ))
+          .user;
+    } on Exception catch (e) {
+      setState(() {
+        _success = false;
+        _errorMessage = e;
+        print(e);
+        user = null;
+        print("there was an exception");
+      });
     }
 
-    void _validate() async {
-        try {
-            if (confirmationResult != null) {
-                await confirmationResult.confirm(_otpController.text);
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                );
-            }
-        } on Exception catch (e) {
-            setState(() {
-            _errorMessage = e;
-            print(e);
-            print("there was an exception");
-            });
-        }
+    if (_success) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    } else {
+      _success = false;
     }
 
-    @override
-    Widget build(BuildContext context) {
-        return Form(
-            key: _formKey,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                    Visibility(
-                        visible: !_otpSent,
-                        child: [
-                            TextFormField(
-                                controller: _mobileController,
-                                decoration: const InputDecoration(labelText: 'Mobile Number'),
-                                validator: (String? value) {
-                                    if (value!.isEmpty) {
-                                        return 'Please enter some text';
-                                    }
-                                    return null;
-                                },
-                            ),
-                            Container(
-                                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                                alignment: Alignment.center,
-                                child: ElevatedButton(
-                                    onPressed: () async {
-                                        if (true) {
-                                            _login();
-                                        }
-                                    },
-                                    child: const Text('Login'),
-                                    style: ElevatedButton.styleFrom(
-                                        primary: Colors.blue, // background
-                                        onPrimary: Colors.white, // foreground
-                                    ),
-                                    
-                                ),
-                            )
-                        ]
-                    ),
-                    Visibility(
-                        visible: _otpSent,
-                        child: [
-                            TextFormField(
-                                controller: _otpController,
-                                decoration: const InputDecoration(labelText: 'One-Time Password'),
-                                validator: (String? value) {
-                                    if (value!.isEmpty) {
-                                        return 'Please enter the OTP';
-                                    }
-                                    return null;
-                                },
-                            ),
-                            Container(
-                                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                                alignment: Alignment.center,
-                                child: ElevatedButton(
-                                    onPressed: () async {
-                                        if (true) {
-                                            _validate();
-                                        }
-                                    },
-                                    child: const Text('Validate'),
-                                    style: ElevatedButton.styleFrom(
-                                        primary: Colors.blue, // background
-                                        onPrimary: Colors.white, // foreground
-                                    ),
-                                    
-                                ),
-                            )
-                        ]
-                    ),
-                ]
-            )
-        );
-    }
+    // await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          TextFormField(
+            controller: _emailController,
+            decoration: const InputDecoration(labelText: 'Email'),
+            validator: (String? value) {
+              if (value!.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: const InputDecoration(labelText: 'Password'),
+            validator: (String? value) {
+              if (value!.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            alignment: Alignment.center,
+            child: ElevatedButton(
+              onPressed: () async {
+                if (true) {
+                  _login();
+                }
+              },
+              child: const Text('Submit'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue, // background
+                onPrimary: Colors.white, // foreground
+              ),
+            ),
+          )
+        ]));
+  }
 }

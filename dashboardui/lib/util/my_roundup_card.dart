651277d694696573cf0e9bcd8c2ae8cc6b33c02b
Brian Lee <briancmlee23@gmail.com>
2022-12-19 02:09:40 +1100

    Handle null date value for cards

--- dashboardui/lib/util/my_roundup_card.dart
+++ dashboardui/lib/util/my_roundup_card.dart
@@ -3,40 +3,46 @@ import 'package:flutter/material.dart';
 import 'package:dashboardui/util/my_card.dart';
 
 class MyRoundupCard extends StatelessWidget {
-    final String id;
-    final color;
+  final String id;
+  final color;
 
-    const MyRoundupCard({
-        Key? key,
-        required this.id,
-        required this.color,
-    }) : super(key: key);
+  const MyRoundupCard({
+    Key? key,
+    required this.id,
+    required this.color,
+  }) : super(key: key);
 
-    DateTime fromTimestampToDateTime(Timestamp timestamp) {
-        return DateTime.fromMicrosecondsSinceEpoch(timestamp.microsecondsSinceEpoch);
+  DateTime? fromTimestampToDateTime(Timestamp? timestamp) {
+    if (timestamp == null) {
+      return null;
     }
+    return DateTime.fromMicrosecondsSinceEpoch(
+        timestamp.microsecondsSinceEpoch);
+  }
 
-    @override
-    Widget build(BuildContext context) {
-        return StreamBuilder(
-            stream: FirebaseFirestore.instance.collection('users').doc(id).snapshots(),
-            builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                return MyCard(
-                    titleText: 'Accrued Roundup',
-                    amount: 'Loading',
-                    date: DateTime.now(),
-                    color: color,
-                );
-                } else {
-                return MyCard(
-                    titleText: 'Accrued Roundup',
-                    amount: snapshot.data!['roundup']['nextDebit']['accAmount'].toString(),
-                    date: fromTimestampToDateTime(snapshot.data!['roundup']['nextDebit']['lastChecked']),
-                    color: color,
-                );
-                }
-            }
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream:
+            FirebaseFirestore.instance.collection('users').doc(id).snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return MyCard(
+              titleText: 'Accrued Roundup',
+              amount: 'Loading',
+              date: DateTime.now(),
+              color: color,
+            );
+          } else {
+            return MyCard(
+              titleText: 'Accrued Roundup',
+              amount: snapshot.data!['roundup']['nextDebit']['accAmount']
+                  .toString(),
+              date: fromTimestampToDateTime(
+                  snapshot.data!['roundup']['nextDebit']['lastChecked']),
+              color: color,
+            );
+          }
+        });
+  }
 }
